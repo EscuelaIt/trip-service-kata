a0197d900f420faaecde3067e66fc1b8aa7ea287
RGV <ricardo.guzman.velasco@outlook.com>
2025-06-30 12:11:21 +0200

    docs: first test to be done

--- c#-dotnet-5/TripServiceKata/TripServiceKataTest/TripServiceTest.cs
+++ c#-dotnet-5/TripServiceKata/TripServiceKataTest/TripServiceTest.cs
@@ -6,6 +6,7 @@ namespace TripServiceKata.Tests
 {
     public class TripServiceTest
     {
+        // Step 1: the easiest test is the one from the not logged in user
         [Fact]
         public void Foo()
         {
